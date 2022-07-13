1b52ec8aca7aa8c454ca013f1e2acc67c97a4d2e
agtp <agtp@protonmail.com>
2022-07-13 22:50:25 +0300

    added links

--- src/compenents/Portfolio.jsx
+++ src/compenents/Portfolio.jsx
@@ -17,27 +17,35 @@ const Portfolio = () => {
 
         {
             id: 1, 
-            src: GenAxia
+            src: GenAxia,
+            lnk: 'https://github.com/108mason/Axia-General'
+
         },
         {
             id: 2, 
-            src: Go2films
+            src: Go2films,
+           
         },
         {
             id: 3, 
             src: Weatherapp
+            
         },
         {
             id: 4, 
-            src: nopcomm
+            src: nopcomm,
+            lnk: 'https://github.com/108mason/FullStackAutomationProject'
+            
         },
         {
             id: 5, 
             src: todolist
+            
         },
         {
             id: 6, 
             src: reactWeather
+           
         },
 
     ]
@@ -56,7 +64,7 @@ const Portfolio = () => {
 
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
             {
-                Portfolios.map(({id, src}) => (
+                Portfolios.map(({id, src, lnk}) => (
                     <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                     <img
                      src={src}
@@ -64,8 +72,8 @@ const Portfolio = () => {
                       className="rounded-md duration=200 hover:scale-105"
                        />
                     <div className="flex items-center justify-center">
-                        <button className="w-1/2 px-6 py-3 m-4 duration-200 hover-scale-105" >Demo</button>
-                        <button className="w-1/2 px-6 py-3 m-4 duration-200 hover-scale-105">Code</button>
+                        <a  className="w-1/2 px-6 py-3 m-4 duration-200 hover-scale-105" >Demo</a>
+                        <a href={lnk} className="w-1/2 px-6 py-3 m-4 duration-200 hover-scale-105">Code</a>
                     </div>
             </div>
                 ))
