412b32033e3ddaf89021a5a53dcabba33e67a8f4
agtp <agtp@protonmail.com>
2022-07-14 12:19:11 +0300

    .

--- src/compenents/Portfolio.jsx
+++ src/compenents/Portfolio.jsx
@@ -72,7 +72,7 @@ const Portfolio = () => {
                       className="rounded-md duration=200 hover:scale-105"
                        />
                     <div className="flex items-center justify-center">
-                        <a  className="w-1/2 px-6 py-3 m-4 duration-200 hover-scale-105" >Demo</a>
+                        <button  className="w-1/2 px-6 py-3 m-4 duration-200 hover-scale-105" >Demo</button>
                         <a href={lnk} className="w-1/2 px-6 py-3 m-4 duration-200 hover-scale-105">Code</a>
                     </div>
             </div>
