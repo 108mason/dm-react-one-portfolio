a8e8a1a0910ab6c6d4ad0aded50ca4d6aed8065e
agtp <agtp@protonmail.com>
2022-07-14 12:26:43 +0300

    .

--- src/compenents/Home.jsx
+++ src/compenents/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
             <div className="flex flex-col justify-center h-full">
                 <h2 className="text-4xl sm:text-7xl font-bold text-white">Frontend Developer | Full Stack Automation Engineer</h2>
-                <p className="text-gray-500 py-4 max-w-md">bla bl sfsdfsdfsd      dsfsdf           dsfsdf a</p>
+                <p className="text-gray-500 py-4 max-w-md"></p>
                 <div>
                     <Link to="portfolio"
                      smooth duration={500}
