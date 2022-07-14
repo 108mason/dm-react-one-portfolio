ad263296f1fa22774af39466dc156f6797f4eac8
agtp <agtp@protonmail.com>
2022-07-14 13:01:08 +0300

    .

--- src/compenents/Contact.jsx
+++ src/compenents/Contact.jsx
@@ -5,8 +5,8 @@ export const Contact = () => {
     <div name='contact' className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white" >
         <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full" >
             <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
-                <p className="py-6">Submit the form below to get in touch with me</p>
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500 hidden sm:block">Contact</p>
+                <p className="py-6 hidden sm:block">Submit the form below to get in touch with me</p>
             </div>
             <div className="flex justify-center items-center">
                 <form action="https://getform.io/f/421b5aaa-d1f1-4f78-b0c2-73ccdb4a3fe5"
