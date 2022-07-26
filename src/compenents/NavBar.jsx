7bfd69710c1132ae1be14673f177584fdf6613fd
108mason <105851623+108mason@users.noreply.github.com>
2022-07-26 14:30:32 +0300

    Update NavBar.jsx

--- src/compenents/NavBar.jsx
+++ src/compenents/NavBar.jsx
@@ -35,7 +35,7 @@ import { Link } from "react-scroll";
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
     <div>
-        <h1 className="text-5xl font-signature ml-2">Daniel Maosn</h1>
+        <h1 className="text-5xl font-signature ml-2">Daniel Mason</h1>
         </div>
 
 <ul className="hidden md:flex">
